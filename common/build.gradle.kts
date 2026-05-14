6c0e008ec8959526f74143909b63f0b3db68f258
kjylab <yupgod@gmail.com>
2026-05-14 11:34:37 +0900

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,16 +11,17 @@ group = "com.github.kjylab"
 version = "1.0.0"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("com.querydsl:querydsl-jpa:5.1.0:jakarta")
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
-
-    runtimeOnly("com.h2database:h2")
-
-    implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+    // kapt와 annotation-api는 빌드 시점에만 필요하므로 그대로 유지
     kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
     kapt("jakarta.persistence:jakarta.persistence-api")
     kapt("jakarta.annotation:jakarta.annotation-api")
+    
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("com.h2database:h2")
+
 }
 
 sourceSets {
